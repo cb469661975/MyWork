a183a601822a055df70d0be15e1fa5ae42a92542
chengbiao <chengbiao@ylb.net>
2018-07-03 21:22:08 +0800

    update_footer

--- app/src/main/java/com/example/cheng/myapplication/ui/YLBRefreshFooter.kt
+++ app/src/main/java/com/example/cheng/myapplication/ui/YLBRefreshFooter.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.animation.*
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
@@ -17,12 +18,20 @@ import com.scwang.smartrefresh.layout.api.RefreshKernel
 import com.scwang.smartrefresh.layout.api.RefreshLayout
 import com.scwang.smartrefresh.layout.constant.RefreshState
 import com.scwang.smartrefresh.layout.constant.SpinnerStyle
+import kotlinx.android.synthetic.main.ylb_loadmore_footer.view.*
 
 /**
  * Created by chengbiao on 2018/3/13.
  */
 
 class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.RefreshFooter {
+    override fun setNoMoreData(noMoreData: Boolean): Boolean {
+        return false
+    }
+
+    override fun onReleased(refreshLayout: RefreshLayout?, height: Int, extendHeight: Int) {
+    }
+
     private var mFooterIv: ImageView? = null
     private var mFooterTvMsg: TextView? = null
 
@@ -48,15 +57,28 @@ class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.Refresh
         initView(context)
     }
 
+    private lateinit var roate: RotateAnimation
+    private lateinit var scale: ScaleAnimation
+
     private fun initView(context: Context) {
         LayoutInflater.from(context).inflate(R.layout.ylb_loadmore_footer, this)
         mFooterIv = findViewById(R.id.footer_iv)
         mFooterTvMsg = findViewById(R.id.footer_tv_msg)
+
+        roate = RotateAnimation(-10f, 10f, Animation.RELATIVE_TO_SELF, 0.2f, Animation.RELATIVE_TO_SELF, 1f)
+        roate.duration = ANIMATION_DUR.toLong()
+        roate.interpolator = LinearInterpolator( )
+        roate.repeatCount = Animation.INFINITE
+        roate.repeatMode = Animation.REVERSE
+
+        scale = ScaleAnimation(1f, 0f, 1f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        scale.duration = ANIMATION_DUR.toLong()
     }
 
-    override fun onPullingUp(percent: Float, offset: Int, footerHeight: Int, extendHeight: Int) {
+    override fun onPulling(percent: Float, offset: Int, footerHeight: Int, extendHeight: Int) {
         //        printLog("up,percent:" + percent + ",offset:" + offset + ",footerHeight:" + footerHeight + ",extendHeight:" + extendHeight);
         if (percent <= 1) {
+            ll_refresh_bottom.visibility = View.VISIBLE
             mFooterIv!!.scaleX = percent
             mFooterIv!!.scaleY = percent
             mFooterTvMsg!!.scaleX = percent
@@ -64,13 +86,9 @@ class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.Refresh
         }
     }
 
-    override fun onPullReleasing(percent: Float, offset: Int, footerHeight: Int, extendHeight: Int) {
+    override fun onReleasing(percent: Float, offset: Int, footerHeight: Int, extendHeight: Int) {
         //        printLog("release,percent:" + percent + ",offset:" + offset + ",footerHeight:" + footerHeight + ",extendHeight:" + extendHeight);
 
-    }
-
-    override fun setLoadmoreFinished(finished: Boolean): Boolean {
-        return false
     }
 
     override fun getView(): View {
@@ -96,7 +114,7 @@ class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.Refresh
     override fun onStartAnimator(layout: RefreshLayout, height: Int, extendHeight: Int) {}
 
     override fun onFinish(layout: RefreshLayout, success: Boolean): Int {
-        return 0
+        return ANIMATION_DUR
     }
 
     override fun isSupportHorizontalDrag(): Boolean {
@@ -104,34 +122,36 @@ class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.Refresh
     }
 
     override fun onStateChanged(refreshLayout: RefreshLayout, oldState: RefreshState, newState: RefreshState) {
+        printLog("PullToUpLoad，oldState：$oldState,newState$newState")
         when (newState) {
             RefreshState.None -> {
                 mFooterTvMsg!!.text = REFRESH_FOOTER_ALLLOADED
-                if (oldState != RefreshState.LoadFinish) {
                     loadGifToImageView(R.drawable.pull_up)
-                }
-                printLog("None，oldState：$oldState,newState$newState")
                 mFooterTvMsg!!.text = REFRESH_FOOTER_PULLUP
-                //                mFooterIv.setGifResource(R.drawable.pull_up);
-                printLog("PullToUpLoad，oldState：$oldState,newState$newState")
             }
-            RefreshState.PullToUpLoad -> {
+            RefreshState.PullUpToLoad -> {
                 mFooterTvMsg!!.text = REFRESH_FOOTER_PULLUP
-                printLog("PullToUpLoad，oldState：$oldState,newState$newState")
+//                printLog("PullToUpLoad，oldState：$oldState,newState$newState")
             }
             RefreshState.Loading -> {
                 //                mFooterIv.setGifResource(R.drawable.pull_up_pic);
                 mFooterTvMsg!!.text = REFRESH_FOOTER_LOADING
-                printLog("Loading")
+//                printLog("Loading")
                 loadGifToImageView(R.drawable.pull_up_pic)
+                mFooterIv?.startAnimation(roate)
             }
             RefreshState.ReleaseToLoad -> {
-                printLog("ReleaseToLoad")
+//                printLog("ReleaseToLoad")
                 mFooterTvMsg!!.text = REFRESH_FOOTER_RELEASE
             }
-            RefreshState.Refreshing -> {
-                printLog("Refreshing")
-                mFooterTvMsg!!.text = REFRESH_FOOTER_REFRESHING
+            RefreshState.LoadFinish -> {
+//                printLog("LoadFinish")
+                mFooterIv?.startAnimation(scale)
+                mFooterTvMsg?.startAnimation(scale)
+                postDelayed({
+                    ll_refresh_bottom.visibility = View.INVISIBLE
+                    reset()
+                }, ANIMATION_DUR.toLong())
             }
         }
     }
@@ -147,16 +167,25 @@ class YLBRefreshFooter : FrameLayout, com.scwang.smartrefresh.layout.api.Refresh
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
 
+
+    private fun reset() {
+        mFooterTvMsg?.scaleX = 1f
+        mFooterTvMsg?.scaleY = 1f
+        mFooterIv?.scaleX = 1f
+        mFooterIv?.scaleY = 1f
+        mFooterIv?.rotation = 0f
     }
 
     companion object {
-        var REFRESH_FOOTER_PULLUP = "上拉加载更多"
-        var REFRESH_FOOTER_RELEASE = "释放立即加载"
-        var REFRESH_FOOTER_LOADING = "正在加载..."
-        var REFRESH_FOOTER_REFRESHING = "正在刷新..."
+        var REFRESH_FOOTER_PULLUP = "加载更多"
+        var REFRESH_FOOTER_RELEASE = "立即加载"
+        var REFRESH_FOOTER_LOADING = "正在加载"
+        var REFRESH_FOOTER_REFRESHING = "正在刷新"
         var REFRESH_FOOTER_FINISH = "加载完成"
         var REFRESH_FOOTER_FAILED = "加载失败"
-        var REFRESH_FOOTER_ALLLOADED = "全部加载完成"
+        var REFRESH_FOOTER_ALLLOADED = "加载完成"
+        private const val ANIMATION_DUR = 500
     }
 }
